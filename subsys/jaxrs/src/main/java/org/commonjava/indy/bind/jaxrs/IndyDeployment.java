1a57bba50f9b3bc09931cb8d473a5e4a1659c1f5
John Casey <jdcasey@commonjava.org>
2021-10-12 17:39:07 -0500

    Minor adjustments to o11yphant project structure to accommodate needs of Quarkus-based projects.

--- subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/IndyDeployment.java
+++ subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/IndyDeployment.java
@@ -30,7 +30,7 @@ import org.commonjava.indy.bind.jaxrs.util.RequestScopeListener;
 import org.commonjava.indy.conf.UIConfiguration;
 import org.commonjava.indy.stats.IndyVersioning;
 import org.commonjava.indy.subsys.honeycomb.TraceManagerProducer;
-import org.commonjava.o11yphant.metrics.GoldenSignalsFilter;
+import org.commonjava.o11yphant.metrics.servlet.GoldenSignalsFilter;
 import org.commonjava.o11yphant.trace.servlet.TraceFilter;
 import org.jboss.resteasy.plugins.server.servlet.HttpServlet30Dispatcher;
 import org.jboss.resteasy.spi.ResteasyDeployment;
